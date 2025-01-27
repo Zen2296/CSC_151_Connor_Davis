678500ebfe0d244155e1842b25963855ba57df54
SANDHILLS\LH218-16 <jad.talbert@gmail.com>
2025-01-27 11:50:14 -0500

    added for changing the clone repo

---
+++ src/labs/example/hello/HelloWorld.java
@@ -0,0 +1,14 @@
+/*
+@author: Connor Davis
+@Date: 1/27/2025
+@purpose: My First Java Program
+
+Note: This program should reside in the directory path as follows ->   CSC_151_Connor_Davis\src\labs\example
+*/
+package labs.example.hello;
+
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/labs/example/mammal/Mammals.java
@@ -0,0 +1,54 @@
+/*
+@author: Connor Davis
+@Date: 1/27/2025
+@Purpose: implemented mammal class given the base methods and properties
+
+*/
+package labs.example.mammals;
+
+public class Mammals {
+
+    protected String hairColor;
+    protected String eyeColor;
+    protected String bodyTemp;
+    protected String legLength;
+    protected String eyeSize;
+    protected String tongueTexture;
+    protected String name;
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void run(){
+       
+    }
+
+    public void eat(){
+      
+    }
+
+    public void sleep(){
+       
+    }
+
+    public void fight(){
+
+    }
+
+    public void drink(){
+      
+    }
+
+    public void blink(){
+     
+    }
+
+    public void sniff(){
+
+    }
+
+    public void sit(){
+        
+    }
+}
