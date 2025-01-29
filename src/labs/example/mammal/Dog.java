833b6e74722714d341d92a4b97dc9952997bd28c
SANDHILLS\LH218-16 <jad.talbert@gmail.com>
2025-01-29 11:46:27 -0500

    adding 2 new classes for the mammal class

---
+++ src/labs/example/mammal/Dog.java
@@ -0,0 +1,27 @@
+/*
+@author: Connor Davis
+@date: 1/29/2025
+@Purpose: Dog class to go along with mammal class
+*/
+
+package labs.example.mammals;
+
+public class Dog extends Mammal{
+
+
+    public void Dog(){
+
+    }
+
+    public void bark(){
+        System.out.println("woof........woof........woof");
+    }
+
+    public void fetch(){
+
+    }
+
+    public void pant(){
+        
+    }
+}
---
+++ src/labs/example/mammal/Main.java
@@ -0,0 +1,9 @@
+/* 
+@author: Connor Davis
+@date: 1/29/2025
+@Purpose: Main class to call for the mammal class
+*/
+
+public class Main{
+
+}
